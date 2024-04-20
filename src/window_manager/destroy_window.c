64c3c12fe9fe8a2a38ce3d7cafeffd8bd3b2deab
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-04-20 23:46:36 +0200

    [+] adding a new destroy_window with verification if it is null

--- src/window_manager/destroy_window.c
+++ src/window_manager/destroy_window.c
@@ -9,7 +9,16 @@
 
 void destroy_window(rpg_t *main)
 {
-    sfRenderWindow_destroy(main->window->window);
-    free(main->window->title);
-    free(main->window);
+    if (main != NULL && main->window != NULL) {
+        if (main->window->window != NULL) {
+            sfRenderWindow_destroy(main->window->window);
+            main->window->window = NULL;
+        }
+        if (main->window->title != NULL) {
+            free(main->window->title);
+            main->window->title = NULL;
+        }
+        free(main->window);
+        main->window = NULL;
+    }
 }
