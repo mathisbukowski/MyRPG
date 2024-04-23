7d6c56ef7e4e9cd2c728e86f66ba20dc5b7dffaa
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-04-23 09:18:30 +0200

    [+] adding a function to add event in a linkedlist

---
+++ src/events/init_event.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** rpg-dev
+** File description:
+** init_event.c
+*/
+
+#include "../../include/rpg.h"
+
+void add_event_to_list(rpg_t *main)
+{
+    event_t *current = main->events;
+
+    if (current == NULL)
+        return;
+
+}
