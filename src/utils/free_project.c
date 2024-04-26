ccd5c16ea39bca1aa0546a24f4afa188036828aa
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-04-26 11:37:22 +0200

    [-] remove the sprite destroy here

--- src/utils/free_project.c
+++ src/utils/free_project.c
@@ -77,6 +77,7 @@ void free_rpg(rpg_t *rpg)
         free_quests(rpg->quests);
         free_events(rpg->events);
         free_keymap(rpg->keymap);
+        free_sprites(rpg->sprites);
         free(rpg);
     }
 }
