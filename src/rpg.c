1df6e0f5a32cfa84103c522ff7fbeee26f761ad7
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-04-27 22:52:11 +0200

    [~] testing and editing the add_entity function to adding background

--- src/rpg.c
+++ src/rpg.c
@@ -12,6 +12,8 @@ void screen_manager(rpg_t *main)
 {
     add_event_to_list(main, &close_window, sfEvtClosed);
     add_key_to_keymap(&(main->keymap), sfKeyEscape, &close_window);
+    add_entity_to_list(main, (entity_params_t){"background", (sfVector2f){0,0}, BACKGROUND}, "assets/bg.jpg");
+    add_entity_to_list(main, (entity_params_t){"continue", (sfVector2f){600,600}, BUTTON_MENU}, "assets/components/continue.png");
     while (sfRenderWindow_isOpen(main->window->window)) {
         event_manager(main);
         display_window(main);
