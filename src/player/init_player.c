7b52d36c5d1a28a0b8c61d9369476e0372648f33
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-05-23 18:16:08 +0200

    [~] moving a line in the render of the scene

--- src/player/init_player.c
+++ src/player/init_player.c
@@ -11,8 +11,6 @@ void init_player_sprite(rpg_t *main)
     entity_t *player = NULL;
     sfIntRect rect = {320, 128, 64, 64};
 
-    add_entity_to_list(main, (entity_params_t){"player",
-    (sfVector2f){928, 508}, PLAYER}, "assets/player/player.png");
     player = find_entity(main, "player");
     player->pos.x = main->map->width * 16 / 2;
     player->pos.y = main->map->height * 16 / 2;
