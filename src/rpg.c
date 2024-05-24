45cc54d30306ab4645f59c044dbd516b461d9e7a
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-05-24 04:36:03 +0200

    [~] isolating the background of entities

--- src/rpg.c
+++ src/rpg.c
@@ -7,14 +7,21 @@
 
 #include "rpg.h"
 
+void define_background(rpg_t *main)
+{
+    main->background_texture = sfTexture_createFromFile("assets/bg.jpg", NULL);
+    main->background_sprite = sfSprite_create();
+    if (check_texture(main->background_sprite, main->background_texture) != 0)
+        return;
+}
+
 void screen_manager(rpg_t *main)
 {
     add_event_to_list(main, &close_window, sfEvtClosed);
     add_key_to_keymap(&(main->keymap), sfKeyEscape, &close_window);
-    add_entity_to_list(main, (entity_params_t){"background",
-    (sfVector2f){0, 0}, BACKGROUND}, "assets/bg.jpg");
     add_entity_to_list(main, (entity_params_t){"player",
     (sfVector2f){928, 508}, PLAYER}, "assets/player/player.png");
+    define_background(main);
     init_start_scene(main);
     init_params_scene(main);
     init_game_scene(main);
